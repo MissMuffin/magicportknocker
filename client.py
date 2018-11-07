0c5c754ebc7d2c6008bfc5013d3afe0c8e868cbe
Miss Muffin <fyyree@gmail.com>
2018-11-07 19:56:52 +0100

    working initial client server with udp and ticket verification

--- client.py
+++ client.py
@@ -1,8 +1,32 @@
+import socket
+import sys
 import auth
+import packet
 
-seed = "skdfnksdjfn"
-number_of_tickets = 10
+secret = "dgkndklfjngkdlfjnk"
+#  one less than the server
+number_of_tickets = 99
+tickets = auth.generate_tokens(secret, number_of_tickets)
 
-tickets = auth.generate_tokens(seed, number_of_tickets)
+ip = "1"
+server_address = ('localhost', 10000)
+p = packet.Packet(tickets[-1], ip)
+buffer = p.pack(secret)
 
-print(tickets[-1])
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+try:
+    # Send data
+    print('sending {!r}'.format(buffer))
+    for i in range(3):
+        sock.sendto(buffer, server_address)
+
+    # Receive response
+    print('waiting for response')
+    data, server = sock.recvfrom(4096)
+    print('received {!r}'.format(data))
+
+finally:
+    print('closing socket')
+    sock.close()
--- server.py
+++ server.py
@@ -1,8 +1,40 @@
+import socket
+import sys
+import packet
 import auth
 
-seed = "skdfnksdjfn"
-number_of_tickets = 10
+secret = "dgkndklfjngkdlfjnk"
+number_of_tickets = 100
+ticket = auth.generate_nth_token(secret, number_of_tickets)
 
-ticket = auth.generate_nth_token(seed, number_of_tickets)
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-print(ticket)
+# Bind the socket to the port
+server_address = ('localhost', 10000)
+print('listening on {} port {}'.format(*server_address))
+sock.bind(server_address)
+
+while True:
+    print('\nlistening...')
+    buffer, address = sock.recvfrom(4096) # cant trust this address
+
+    # unpack and verify signature
+    unpacked_buffer = packet.Packet.unpack(secret, buffer)
+    client_ticket = unpacked_buffer.ticket
+    client_ip = unpacked_buffer.ip
+
+    # check for correct ticket
+    if auth.verify_ticket(client_ticket, ticket):
+
+        ticket = client_ticket
+
+        print('received {} bytes from {}'.format(len(buffer), address))
+        print("ticket was correct")
+        print("client ip:", client_ip)
+
+        sent = sock.sendto("opened port".encode(), address)
+        print('sent {} bytes back to {}'.format(sent, address))
+    
+    else:
+        print("authentication failed")
