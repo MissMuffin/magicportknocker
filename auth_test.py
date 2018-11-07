bfa433457bf8b4e97fc00e6d01d5505fc227fbc3
Miss Muffin <fyyree@gmail.com>
2018-11-07 18:49:50 +0100

    refactored

--- auth_test.py
+++ auth_test.py
@@ -12,10 +12,10 @@ def test_verify():
     n = 12
     token_client = generate_tokens(seed, n)[-2]
     token_server = generate_nth_token(seed, n)
-    assert verify(token_client, token_server)
+    assert verify_ticket(token_client, token_server)
 
 def test_verify_bad():
     n = 12
     token_client = generate_tokens(seed, n)[-1]
     token_server = generate_nth_token(seed, n)
-    assert not verify(token_client, token_server)
+    assert not verify_ticket(token_client, token_server)
