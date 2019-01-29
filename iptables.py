8d88bcf3bf98bef297e664e8743d7a954f123b50
Miss Muffin <fyyree@gmail.com>
2019-01-29 22:55:47 +0100

    script to temporarily open privilieged ports for authenticated users

---
+++ iptables.py
@@ -0,0 +1,26 @@
+import iptc
+from elevate import elevate
+
+# add user port privileges to iptables (after successful authentication)
+def open_ports(ip, ports):
+    table = iptc.Table(iptc.Table.FILTER)
+    chain = iptc.Chain(table, "INPUT")
+    for port in ports:
+        for proto in ["tcp", "udp"]:
+            rule = iptc.Rule()
+            rule.src = ip
+            rule.protocol = proto
+            rule.target = rule.create_target("ACCEPT")
+            m = rule.create_match(proto)
+            m.dport = port
+            try:
+                chain.delete_rule(rule)
+            except:
+                pass
+            chain.insert_rule(rule)
+    table.commit()
+
+if __name__ == '__main__':
+    # elevate this to root
+    elevate(graphical=False)
+    open_ports("127.0.0.3", ["44053"])
