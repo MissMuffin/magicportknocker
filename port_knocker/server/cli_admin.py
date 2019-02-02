115d0111376c0ee9b06a4e6b6c7b012f788c9e94
Miss Muffin <fyyree@gmail.com>
2019-02-03 00:46:52 +0100

    add some basic validation on user creation

--- port_knocker/server/cli_admin.py
+++ port_knocker/server/cli_admin.py
@@ -30,18 +30,35 @@ def _show_user_table(users):
     table = AsciiTable(data)
     click.echo(table.table)
 
+def getValidUsername():
+    blacklisted_characters = ['/', '\\', '.']
+    user_name = click.prompt("Enter user name (Spaces allowed)")
+    if any(c in user_name for c in blacklisted_characters): 
+        print("{} contains illegal characters ({})".format(user_name, blacklisted_characters))
+        return getValidUsername()
+    return user_name
+
+def getPortNumbers(user_name):
+    ports = click.prompt("Enter port priviliges for user {} (separate port numbers with space)".format(user_name))
+    ports = ports.split()
+    try:
+        if any(int(port) > 65535 or int(port) < 1 for port in ports):
+            print("Invalid port numbers, needs to be between 1 and 65535")
+            return getPortNumbers(user_name)
+    except ValueError:
+        print("Ports must be integers")
+        return getPortNumbers(user_name)
+    return ports
+
 @main.command()
 def add():
     """Add a user and their privileges."""
-    user_name = click.prompt("Enter user name (Spaces allowed)")
+    user_name = getValidUsername()
     n_tickets = click.prompt("Enter number of tickets for this user", type=int, default=3)
-    print(n_tickets)
-    ports = click.prompt("Enter port priviliges for user {} (separate port numbers with space)".format(user_name))
-    ports = ports.split()
-    # TODO check that port in range?
+    ports = getPortNumbers(user_name)
     state.add_user(user_name=user_name, n_tickets=n_tickets, ports=ports) 
     click.echo("Added {} ticket(s) for user {} with {} priviliges: {}".format(n_tickets, user_name, len(ports), ports))
-
+        
 
 @main.command()
 def view():
