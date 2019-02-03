b4245c0dc80af6d9da43b9e30d251ae378c60370
Miss Muffin <fyyree@gmail.com>
2019-02-03 19:04:20 +0100

    added method to close ports for one ip

--- port_knocker/server/iptables.py
+++ port_knocker/server/iptables.py
@@ -2,7 +2,7 @@ import iptc
 from elevate import elevate
 from port_knocker.util.security_logger import sec_logger
 
-# add user port privileges to iptables (after successful authentication)
+# add user port privileges to iptables
 def open_ports(ip, ports):
     table = iptc.Table(iptc.Table.FILTER)
     chain = iptc.Chain(table, "INPUT")
@@ -20,9 +20,28 @@ def open_ports(ip, ports):
                 pass
             chain.insert_rule(rule)
     table.commit()
-    sec_logger.info("opened ports {}".format(ports))
+    sec_logger.info("opened ports {} for ip {}".format(ports, ip))
+
+def close_ports(ip, ports):
+    table = iptc.Table(iptc.Table.FILTER)
+    chain = iptc.Chain(table, "INPUT")
+    for port in ports:
+        for proto in ["tcp", "udp"]:
+            rule = iptc.Rule()
+            rule.src = ip
+            rule.protocol = proto
+            rule.target = rule.create_target("ACCEPT")
+            m = rule.create_match(proto)
+            m.dport = port
+            try:
+                chain.delete_rule(rule)
+            except:
+                pass #TODO do something else here?
+    table.commit()
+    sec_logger.info("closed ports {} for ip {}".format(ports, ip))
 
 if __name__ == '__main__':
     # elevate this to root
     elevate(graphical=False)
     open_ports("127.0.0.3", ["44053"])
+    close_ports("127.0.0.3", ["44053"])
