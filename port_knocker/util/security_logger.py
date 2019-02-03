a8636ae4f1eb8073b4fc40ca38693324ddb79d9c
Miss Muffin <fyyree@gmail.com>
2019-02-03 19:04:05 +0100

    set logger level to warn

--- port_knocker/util/security_logger.py
+++ port_knocker/util/security_logger.py
@@ -2,9 +2,8 @@ import sys
 import logging
 
 sec_logger = logging.getLogger("security")
-sec_logger.setLevel(logging.WARN)
+sec_logger.setLevel(logging.INFO)
 # Remove default handlers
-print(sec_logger.handlers)
 if not sec_logger.handlers:
     formatter = logging.Formatter(fmt="[%(asctime)s %(levelname)s]: %(message)s")
     file_handler = logging.FileHandler('security.log')
